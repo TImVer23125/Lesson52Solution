b72f788e75ca93076acd7aca287c20cac984655a
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-05-08 18:00:33 +0300

    Update sort algorithm

--- Test01/logic.cpp
+++ Test01/logic.cpp
@@ -1,24 +1,25 @@
 #include "logic.h"
-#include "util.h"
 
 void sort_bubble(int array[], int size) {
-	int count = 0;
 	if (size <= 0) {
 		return;
 	}
 
 	for (int i = 0; i < size - 1; i++)
 	{
+		bool flag = true;
 		for (int j = 0; j < size - 1 - i; j++)
 		{
-			count++;
 			if (array[j] > array[j + 1]) {
+				flag = false;
 				int t = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = t;
 			}
 		}
+		if (flag) {
+			break;
+		}
 	}
 
-	cout << "count = " << count << endl;
 }
