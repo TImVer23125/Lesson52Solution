49765a6fdedc9914f4af693625509f00b3e3bc4a
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-05-08 17:48:54 +0300

    update sort bubble

--- Test01/logic.cpp
+++ Test01/logic.cpp
@@ -1,14 +1,17 @@
 #include "logic.h"
+#include "util.h"
 
 void sort_bubble(int array[], int size) {
+	int count = 0;
 	if (size <= 0) {
 		return;
 	}
 
 	for (int i = 0; i < size - 1; i++)
 	{
-		for (int j = 0; j < size - 1; j++)
+		for (int j = 0; j < size - 1 - i; j++)
 		{
+			count++;
 			if (array[j] > array[j + 1]) {
 				int t = array[j];
 				array[j] = array[j + 1];
@@ -16,4 +19,6 @@ void sort_bubble(int array[], int size) {
 			}
 		}
 	}
+
+	cout << "count = " << count << endl;
 }
